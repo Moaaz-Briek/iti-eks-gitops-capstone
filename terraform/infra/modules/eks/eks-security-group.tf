1dc7f006affedcb3084b67035b85081240047ad2
Mohamed Salem <mhmedsalem10@gmail.com>
2025-06-05 15:36:55 +0300

    Updating the eks cluster role

---
+++ terraform/infra/modules/eks/eks-security-group.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "eks" {
+  name        = "${var.cluster_name}-eks-sg"
+  description = "EKS control plane and worker node communication"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Kubernetes API"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-eks-sg"
+  }
+}
