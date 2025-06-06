be0108d64d19f353a4650f9929d648cf9f03d921
DanielFarag <danielfarag146@gmail.com>
2025-06-07 00:50:43 +0300

    chore: use --ignore-not-found, update branch to main, and support dynamic tfvars mapping

--- utils/sh/deploy.sh
+++ utils/sh/deploy.sh
@@ -7,9 +7,9 @@ export AWS_ACCESS_KEY_ID=$(echo "$CREDENTIALS_JSON" | jq -r '.Credentials.Access
 export AWS_SECRET_ACCESS_KEY=$(echo "$CREDENTIALS_JSON" | jq -r '.Credentials.SecretAccessKey')
 export AWS_SESSION_TOKEN=$(echo "$CREDENTIALS_JSON" | jq -r '.Credentials.SessionToken')
 
-kubectl delete -f https://raw.githubusercontent.com/danielfarag/iti-eks-gitops-capstone/feature/deploy-scripts/utils/terraform-as-pod/deploy.yaml
-kubectl delete -f https://raw.githubusercontent.com/danielfarag/iti-eks-gitops-capstone/feature/deploy-scripts/utils/terraform-as-pod/${1}_config.yaml
+kubectl delete -f https://raw.githubusercontent.com/danielfarag/iti-eks-gitops-capstone/main/utils/terraform-as-pod/deploy.yaml --ignore-not-found
+kubectl delete -f https://raw.githubusercontent.com/danielfarag/iti-eks-gitops-capstone/main/utils/terraform-as-pod/${1}_config.yaml --ignore-not-found
 
 
-kubectl apply -f https://raw.githubusercontent.com/danielfarag/iti-eks-gitops-capstone/feature/deploy-scripts/utils/terraform-as-pod/${1}_config.yaml
-kubectl apply -f https://raw.githubusercontent.com/danielfarag/iti-eks-gitops-capstone/feature/deploy-scripts/utils/terraform-as-pod/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/danielfarag/iti-eks-gitops-capstone/main/utils/terraform-as-pod/${1}_config.yaml
+kubectl apply -f https://raw.githubusercontent.com/danielfarag/iti-eks-gitops-capstone/main/utils/terraform-as-pod/deploy.yaml
--- utils/terraform-as-pod/pipeline.sh
+++ utils/terraform-as-pod/pipeline.sh
@@ -1,9 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 [ -z "$REPO_URL" ] && echo "Error: REPO_URL is not set." && exit 1
 [ -z "$TF_PATH" ] && echo "Error: TF_PATH is not set." && exit 1
 [ -z "$BUCKET_NAME" ] && echo "Error: BUCKET_NAME is not set." && exit 1
 [ -z "$BUCKET_STATE" ] && echo "Error: BUCKET_STATE is not set." && exit 1
+
+
+VAR_ARGS=()
+for env_var in $(env | grep '^VAR_' | cut -d= -f1); do
+  var_name=$(echo "$env_var" | sed 's/^VAR_//')
+  var_value=$(printenv "$env_var")
+  VAR_ARGS+=("-var" "${var_name}=${var_value}")
+done
+
 
 REPO_DIR="/tmp/terraform_repo"
 
@@ -26,5 +35,5 @@ EOF
 echo "Running terraform init..."
 terraform init -reconfigure
 
-echo "Running terraform apply..."
-terraform apply -auto-approve
+echo "Running terraform apply with "${VAR_ARGS[@]}" ...."
+terraform apply "${VAR_ARGS[@]}" -auto-approve
