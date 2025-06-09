ce56ab0a65a4581bb670a6381392e5937f560c9f
DanielFarag <danielfarag146@gmail.com>
2025-06-09 19:52:44 +0300

    feature: create bastion server + ssm integration

---
+++ terraform/infra/modules/bastion/iam.tf
@@ -0,0 +1,38 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "bastion" {
+  name               = "bastion-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_policy" "eks_describe_cluster_policy" {
+  name        = "EC2BastionServerAccessPolicties"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "eks:DescribeCluster",
+        Resource = "arn:aws:eks:${var.region}:${data.aws_caller_identity.current.account_id}:cluster/*"
+      },{
+        Effect   = "Allow",
+        Action   = "s3:*",
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "bastion_eks_describe_attachment" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = aws_iam_policy.eks_describe_cluster_policy.arn
+}
+
+
+
+resource "aws_iam_role_policy_attachment" "bastion_ssm_attachment" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
