1d94f7078e8e8d1de6397203640086f9a9fc39e9
Moaaz <moaaz.briek97@gmail.com>
2025-06-12 00:14:02 +0300

    test webhook trigger

--- backend/app.js
+++ backend/app.js
@@ -1,50 +1,50 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-const sequelize = require('./db/sequelize-instance');
-const redis = require('./db/redis');
-const cors = require('cors');
+var express = require('express')
+var path = require('path')
+var logger = require('morgan')
+const sequelize = require('./db/sequelize-instance')
+const redis = require('./db/redis')
+const cors = require('cors')
 
-var postsRouter = require('./routes/posts');
-var indexRouter = require('./routes/index');
-var metricsRouter = require('./routes/metrics');
+var postsRouter = require('./routes/posts')
+var indexRouter = require('./routes/index')
+var metricsRouter = require('./routes/metrics')
 
-var app = express();
+var app = express()
 
 sequelize
-  .authenticate()
-  .then(() => {
-    return sequelize.sync({ alter: true });
-  })
-  .then(() => {
-    console.log('Database synchronized.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-    throw new Error('Database Connection Failed');
-  });
+    .authenticate()
+    .then(() => {
+        return sequelize.sync({ alter: true })
+    })
+    .then(() => {
+        console.log('Database synchronized.')
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err)
+        throw new Error('Database Connection Failed')
+    })
 
 redis.on('connect', () => {
-  console.log('Connected to Redis');
-});
+    console.log('Connected to Redis')
+})
 
 redis.on('ready', () => {
-  console.log('Redis is ready to use');
-});
+    console.log('Redis is ready to use')
+})
 
 redis.on('error', (err) => {
-  console.error('Redis connection failed:', err);
-  throw new Error('Redis Connection Failed');
-});
+    console.error('Redis connection failed:', err)
+    throw new Error('Redis Connection Failed')
+})
 
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use('/', indexRouter)
+app.use('/posts', postsRouter)
+app.use('/metrics', metricsRouter)
 
+module.exports = app
 
-app.use('/', indexRouter);
-app.use('/posts', postsRouter);
-app.use('/metrics', metricsRouter);
-
-module.exports = app;
+// test
